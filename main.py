5e186070fa12e1743cc7196e2be2ffdc3d83b83a
tomoyuki.nohara <nohara@accelstars.com>
2025-08-01 15:49:29 +0900

    refactor main.py to load configuration from YAML file, replacing hardcoded values for device address, token, and device ID

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import asyncio
 from datetime import datetime
 from bleak import BleakClient
 import numpy as np
-import json
-from datetime import timedelta
+import yaml
 
 from data_saver import AsyncDataSaver
 
 RR_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
-# [TODO] 設定ファイルから取得する
-DEVICE_ADDRESS = "ABDA0013-523B-24DC-53F2-E4F934D7BE94"
+# 設定ファイルからロード
+with open("config.yml", "r") as f:
+    cfg = yaml.safe_load(f)
 
-# [TODO] 設定ファイルから取得する
-TOKEN = "f26eaa7007230625ffc25ce78cae8444b62036f9bdd9fb43caa31ae873af6021f3cc0a81a16dac9c13247cc03f686915"
-DEVICE_ID = "6055F92A78EE"
+DEVICE_ADDRESS = cfg["DEVICE_ADDRESS"]
+TOKEN = cfg["TOKEN"]
+DEVICE_ID = cfg["DEVICE_ID"]
 
 data_saver = AsyncDataSaver()
 
@@ -55,21 +55,15 @@ async def run_once(t_interval=60, timeout_sec=10):
 
     try:
         async with BleakClient(DEVICE_ADDRESS) as client:
-            print("[INFO] 接続中...")
             await asyncio.wait_for(client.connect(), timeout=timeout_sec)
             _ = client.services
             await asyncio.wait_for(
                 client.start_notify(RR_UUID, handle_rr_data), timeout=timeout_sec
             )
-            print(f"[INFO] {t_interval}秒取得開始")
-
             await asyncio.sleep(int(t_interval))
-
             await asyncio.wait_for(client.stop_notify(RR_UUID), timeout=timeout_sec)
-            print(f"[INFO] {t_interval}秒取得完了")
 
     except Exception as e:
-        print(f"[ERROR] {e}")
         await asyncio.sleep(3)
 
 
